7792761e6527e4885ebd321b1593506043a96b0e
MacReiben <macreiben@gmail.com>
2023-05-20 09:30:28 +0200

    more tuning on jest extension coverage.

--- src/services/SomeDummyService.js
+++ src/services/SomeDummyService.js
@@ -3,6 +3,10 @@ class SomeDummyService {
     doSomething(left, right) {
         return left + right;
     }
+
+    someUntestedMethod() {
+        return "som_data"
+    }
 }
 
 export default SomeDummyService
