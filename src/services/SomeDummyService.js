03e5e02a643c13dc49fd34bbef71b7ed643461db
MacReiben <macreiben@gmail.com>
2023-05-20 07:59:28 +0200

    added one unit test on a service.

---
+++ src/services/SomeDummyService.js
@@ -0,0 +1,8 @@
+
+class SomeDummyService {
+    doSomething(left, right) {
+        return left + right;
+    }
+}
+
+export default SomeDummyService
--- src/tests/some_tests.spec.js
+++ src/tests/some_tests.spec.js
@@ -1,6 +1,15 @@
+import SomeDummyService from "../services/SomeDummyService";
 
 describe('Some test suite', () => {
     test('Some unit test here', () => {
         expect(1).toBe(1);
+    })
+
+    test('Some unit test that access a service', () => {
+        const target = new SomeDummyService()
+
+        const actual = target.doSomething(1, 2)
+
+        expect(actual).toBe(3)
     })
 })
